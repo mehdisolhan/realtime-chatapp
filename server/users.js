66d4d205b504c4aff310766770dc56aec3c52006
Mehdi Solhan <smehdy@hotmail.com>
2021-03-28 13:04:36 +0300

    creating user helpers and import to server index

---
+++ server/users.js
@@ -0,0 +1,40 @@
+//Helper functions to control users
+
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const existingUser = users.find(
+    (user) => user.room === room && user.name === name
+  );
+
+  if (existingUser) {
+    return { error: "Username is taken" };
+  }
+
+  const user = { id, name, room };
+
+  users.push(users);
+
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+const getUser = () => {
+  return users.find((user) => user.id === id);
+};
+
+const getUsersInRoom = (room) => {
+  return users.filter((user) => user.room === room);
+};
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
